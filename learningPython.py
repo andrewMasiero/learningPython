cab9bbafe4d105e3eebe481ddbc017730dc76f2a
andrewMasiero <50714278+andrewMasiero@users.noreply.github.com>
2022-11-05 10:55:42 -0500

    ch zip and map

--- learningPython.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import text
-from sqlalchemy import create_engine
-
-
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-
-with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
